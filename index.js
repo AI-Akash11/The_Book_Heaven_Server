0e7c7495bef7f4cb49a4272038d403dc8af015d0
Ali Imam Akash <akash1437aliimam@gmail.com>
2026-01-01 19:50:40 +0600

    latest books api created

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const express = require('express')
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yxfp204.mongodb.net/?appName=Cluster0`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,32 +18,39 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
-
-
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const db = client.db("book_heaven_db");
+    const booksCollection = db.collection("books");
 
-    const db = client.db('book_heaven_db');
-    const booksCollection = db.collection('books');
+    // books api----------------------------------------
+    app.get("/books", async (req, res) => {
+      const result = await booksCollection.find().toArray();
+      res.send(result);
+    });
 
-    // books get api----------------------------------------
-    app.get('/books', async(req, res)=>{
-        const result = await booksCollection.find().toArray();
-        res.send(result)
-    })
 
-    
+    // latest-books api----------------------------------
+    app.get("/latest-books", async (req, res) => {
+      const result = await booksCollection
+        .find()
+        .sort({ created_at: -1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -54,12 +58,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('The Book Heaven Is Online!')
-})
+app.get("/", (req, res) => {
+  res.send("The Book Heaven Is Online!");
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
